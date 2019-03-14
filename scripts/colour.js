fed63d10fb496f7e337f15c5e95f9bedb145dddd
Lezzz <valentin.lazureanu@gmail.com>
2019-03-14 23:04:08 +0200

    added more functionality

--- scripts/colour.js
+++ scripts/colour.js
@@ -1,13 +1,7 @@
-var colours = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
+var colours = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
+var header = document.querySelector("h1");
 var pickedColor = pickColour();
 var colourDisplay = document.querySelector("#colourDisplay");
 var messageDisplay = document.querySelector("#message");
@@ -29,6 +23,8 @@ for (var i = 0; i < squares.length; i++) {
         if (clicked === pickedColor) {
             messageDisplay.textContent = "WINNER! CONGRATZ!";
             changeColours(clicked);
+            header.style.backgroundColor = clicked;
+            
         } else {
             this.style.backgroundColor = "#c0f8d1";
             messageDisplay.textContent = "Try again";
@@ -43,8 +39,27 @@ function changeColours(colour) {
     }
 }
 
-function pickColour(){
-var random = Math.floor(Math.random() * colours.length);
-return colours[random];
+function pickColour() {
+    var random = Math.floor(Math.random() * colours.length);
+    return colours[random];
 
+}
+
+function generateRandomColors(num) {
+    //make array
+    var arr = [];
+    //add num randomcolros to array
+    for (var i = 0; i < num; i++) {
+        //get random color and push into arr
+        arr.push(randomColor());
+    }
+    //return array
+    return arr;
+}
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
